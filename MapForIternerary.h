6908a3c59cc408c80c57bdb474d143ca83994ca2
Fan Yang <fanyang@Fans-MacBook-Pro-2.local>
2014-11-07 10:45:04 -0800

    Started work on new map feature, not quite done yet.

---
+++ MapForIternerary.h
@@ -0,0 +1,13 @@
+//
+//  MapForIternerary.h
+//  GitUseExample
+//
+//  Created by Fan Yang on 11/7/14.
+//  Copyright (c) 2014 com.fanyang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapForIternerary : NSObject
+
+@end
---
+++ MapForIternerary.m
@@ -0,0 +1,13 @@
+//
+//  MapForIternerary.m
+//  GitUseExample
+//
+//  Created by Fan Yang on 11/7/14.
+//  Copyright (c) 2014 com.fanyang. All rights reserved.
+//
+
+#import "MapForIternerary.h"
+
+@implementation MapForIternerary
+
+@end
